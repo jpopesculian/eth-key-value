81ef786eae10aac44bd4a774705bda89f68260af
Julian Popescu <jpopesculian@gmail.com>
2018-09-19 16:21:44 +0200

    reading and writing to contract key / values

--- contracts/KeyValueStore.sol
+++ contracts/KeyValueStore.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.24;
 
 
 contract KeyValueStore {
-    mapping (bytes32 => string) public data;
+    mapping (bytes32 => bytes) public data;
     mapping (bytes32 => bool) public created;
 
-    mapping (bytes32 => mapping (address => string[])) public keys;
+    mapping (bytes32 => mapping (address => bytes)) public keys;
 
     mapping (address => string) public registration;
     mapping (address => bool) public registered;
@@ -15,17 +15,26 @@ contract KeyValueStore {
     mapping (bytes32 => address[]) private writers;
     mapping (bytes32 => address[]) public readers;
 
-    function create(address account, bytes32 accessor, string encryptedData, string encryptedKey) public {
-        if (exists(accessor)) {
-            return;
+    function create(address account, bytes32 accessor, bytes encryptedData, bytes encryptedKey) public {
+        if (created[accessor]) {
+            // return;
         }
         created[accessor] = true;
         data[accessor] = encryptedData;
         owners[accessor] = [account];
+        keys[accessor][account] = encryptedKey; // move to grantReadAccess
     }
 
-    function exists(bytes32 accessor) public view returns(bool) {
-        return created[accessor];
+    function write(bytes32 accessor, bytes encryptedData) public {
+        data[accessor] = encryptedData;
+    }
+
+    function isOwner(address account) public view returns(bool) {
+        return true;
+    }
+
+    function getKey(bytes32 accessor, address account) public returns(bytes) {
+        return keys[accessor][account];
     }
 
     function setRegistration(string publicKey) public {
